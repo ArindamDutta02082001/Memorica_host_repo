cef29bf20b81e1b8c77996335cdf24c023ec93cd
ArindamDutta02082001 <83761396+ArindamDutta02082001@users.noreply.github.com>
2023-07-23 00:31:24 +0530

    files0

---
+++ memorymodel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schemaa = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  Memory: String,
+  name2_printed: String,
+});
+
+const Model = mongoose.model("Memorica", Schemaa);
+module.exports = { Model };
